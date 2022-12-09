a440a8ee864c918bb072cb546da3a906c70e91a0
Jinsu Kim <akrn112@naver.com>
2022-12-10 00:39:29 +0900

    third commit

--- src/main/java/com/example/vo/UserVO.java
+++ src/main/java/com/example/vo/UserVO.java
@@ -1,9 +1,29 @@
 package com.example.vo;
 
+import java.util.Date;
+
 public class UserVO {
+    private String seq;
     private String id;
     private String username;
     private String pwd;
+    private Date regdate;
+
+    public String getSeq() {
+        return seq;
+    }
+
+    public void setSeq(String seq) {
+        this.seq = seq;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
 
     public String getId() {
         return id;
